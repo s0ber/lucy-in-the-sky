0fa3dee5501f736da8cdb5211b90cf68378038ce
Sergey <sergeyshishkalov@gmail.com>
2017-01-23 15:58:36 +0300

    Improve letters animation

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const createSpan = letter => `<span class="letter">${letter}</span>`
 text.innerHTML = text.textContent.trim().split('').map(createSpan).join('')
 const letters = document.querySelectorAll('.letter')
 
+const MOVEMENT_TIME = 600
+
 const getDirection = () => {
   const x = Math.random() * 6 - 3
   const y = Math.random() * 6 - 3
@@ -14,33 +16,58 @@ const normalizeVector = ({x, y}) => {
   return {x: x / arcLength, y: y / arcLength}
 }
 
-const move = () => {
-  letters.forEach((letter) => {
-    const {x, y} = normalizeVector(getDirection())
-    letter.style.transform = `translate(${x}px, ${y}px)`
-  })
-  requestAnimationFrame(move)
+let isWindowActive = true
+window.onfocus = () => { isWindowActive = true }
+window.onblur = () => { isWindowActive = false }
+
+const bringJoy = (letters, movements = []) => {
+  let nextMovements = []
+
+  // do nothing when window loses focus
+  if (isWindowActive) {
+    if (movements.length === 0) {
+      nextMovements = Array.from(letters).map(moveLetter)
+    } else {
+      nextMovements = movements.map(movement => movement())
+    }
+  }
+
+  // reset "next movements", if window loses focus,
+  // so new movements will be scheduled, when window will receive focus next time
+  requestAnimationFrame(() => { bringJoy(letters, nextMovements) })
 }
 
-move()
+const moveLetter = (letter, {startPoint, endPoint, startTime, endTime} = {}) => {
+  const curTime = performance.now()
 
-const lerp = (startPoint, endPoint, period) => {
-  return {
-    x: startPoint.x + (endPoint.x - startPoint.x) * period,
-    y: startPoint.y + (endPoint.y - startPoint.y) * period
+  // prev movement is over
+  if (endTime && curTime >= endTime) {
+    // start from previous "frame", so letter will not stop for a moment
+    return moveLetter(letter, {startPoint: endPoint, startTime: curTime - (1000 / 60)})
+  }
+
+  // very first movement started
+  if (!startPoint) {
+    startPoint = {x: 0, y: 0}
+    // start from random time to make letters move more independent
+    startTime = curTime - (MOVEMENT_TIME * Math.random())
+  }
+
+  // new movement started, schedule new direction
+  if (!endPoint) {
+    endPoint = normalizeVector(getDirection())
+    endTime = startTime + MOVEMENT_TIME
+  }
+
+  const movementPos = (curTime - startTime) / MOVEMENT_TIME
+  const x = startPoint.x + (endPoint.x - startPoint.x) * movementPos
+  const y = startPoint.y + (endPoint.y - startPoint.y) * movementPos
+
+  letter.style.transform = `translate(${x}px, ${y}px)`
+
+  return () => {
+    return moveLetter(letter, {startPoint, startTime, endPoint, endTime})
   }
 }
 
-let letterState = []
-let deltaTime = 0
-let lastTimeStamp = performance.now()
-const animate = () => {
-  const timeStamp = performance.now()
-  deltaTime = timeStamp - lastTimeStamp
-  lastTimeStamp = timeStamp
-
-  letters.forEach((letter, i) => { })
-
-  requestAnimationFrame(animate)
-}
-// animate()
+bringJoy(letters)
